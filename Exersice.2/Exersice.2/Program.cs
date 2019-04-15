8ec74fbc430c666ec8d29f026c485a6943bfcdfd
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-15 15:15:59 +0200

    Add files via upload

---
+++ Exersice.2/Exersice.2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace Exersice._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            var number1 = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine("Enter another number");
+            var number2 = Convert.ToInt16(Console.ReadLine());
+            var max = (number1 > number2) ? number1 : number2;
+            Console.WriteLine("max is"+max);
+
+        }
+    }
+}
